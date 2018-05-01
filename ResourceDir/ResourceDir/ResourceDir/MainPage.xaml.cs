238da13128bf5f3af5eadd0849426e79c9456a21
mohamedg <zeafanm@gmail.com>
2018-05-01 09:13:37 +0200

    the FirstCommit

---
+++ ResourceDir/ResourceDir/ResourceDir/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ResourceDir
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+         void ChangeColor(object sender, System.EventArgs e)
+        {
+           Application.Current.Resources["BackGroundColorBtn"] =Color.Azure;
+        }
+
+    }
+}
